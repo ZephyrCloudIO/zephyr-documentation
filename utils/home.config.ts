93bdaca637f0f64d211e5b5a917daf888217282c
zmzlois <lois.z@comcord.vision>
2024-06-04 11:39:32 +0100

    Reapply "home page wrap"
    This reverts commit 5b5dc97b2194c2509fd397b1c0ea684ffee9f0ce.

--- utils/home.config.ts
+++ utils/home.config.ts
@@ -11,25 +11,28 @@ export type CardProps = Record<string, CardItemProps[]>;
 export const HomeConfig: CardProps = {
     start: [
         {
-            "title": "Why Zephyr Cloud", // TODO: shouldn't introduction and "Why" be on the same page?
-            "href": "/why-zephyr-cloud",
+            "title": "Why Zephyr", // TODO: shouldn't introduction and "Why" be on the same page?
+            "description": "Have an overview of modern application deployment's complexity and how Zephyr can speed up the process.",
+            "href": "/guide/getting-started/why-zephyr-cloud",
 
         },
         {
-            "title": "Install Zephyr",
-            "href": "/introduction",
+            "title": "Adding Zephyr to existing application",
+            "description": "One line of code to add Zephyr to your existing application.",
+            "href": "/guide/getting-started/existing-app",
             "variant": "small"
         },
         {
-            "title": "Create Module Federation App",
-            "href": "/mental-model",
+            "title": "Module Federation Application",
+            "description": "Add Zephyr to applications built on module federation.",
+            "href": "/guide/getting-started/create-mf-app",
             "variant": "small"
         },
     ],
     configure: [
         {
             "title": "Migrate from vanilla module federation",
-            href: "/installation",
+            href: "/guide/getting-started/vanilla-mf",
             "variant": "small"
         },
         {
@@ -42,11 +45,7 @@ export const HomeConfig: CardProps = {
             href: "/migrating-from-medusa",
             "variant": "small"
         },
-        {
-            "title": "Migrating from Module Federation",
-            href: "/migrating-from-module-federation",
-            description: "Creating a module federation application",
-            "variant": "small"
-        }
-    ]
+
+    ],
+    receipes: []
 }
