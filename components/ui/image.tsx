1d1a63ae680c910e9d0cb9e068aae2ecfa2713de
zmzlois <lois.z@comcord.vision>
2024-06-09 18:24:19 +0100

    style: dynamic size of image component

--- components/ui/image.tsx
+++ components/ui/image.tsx
@@ -1,7 +1,11 @@
-export const Image = ({ src }: { src?: string }) => {
+import { cn } from "../../utils/cn";
+
+export const Image = ({ src, size }: { src?: string, size?: "medium" | "large" }) => {
+
+  const _size = size === "large" ? "w-2/3" : "w-1/3";
   return (
-    <div className="items-center flex justify-center">
-      <img src={src} className="w-1/3 rounded-lg" />
+    <div className="flex items-center justify-center py-2 lg:py-4">
+      <img src={src} className={cn(" rounded-lg", _size )} />
     </div>
   );
 };
