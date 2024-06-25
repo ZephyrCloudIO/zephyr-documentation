efa545fdb8f48f0b94702e356c7d88dfbe37bc94
zmzlois <lois.z@comcord.vision>
2024-06-25 08:13:54 +0100

    feat(docs): change error code

--- lib/error.ts
+++ lib/error.ts
@@ -1,30 +1,67 @@
 export const Errors = {
+    // build specific errors
     build: [
         {
-            code: "NG011",
-            name: "Expression changed after checked",
-            link: "/error/ng011"
-        },
-        {
-            code: "ZE001",
+            code: "ZE10010",
             name: "Package.json not found.",
-            link: "/error/ze001"
+            link: "/guide/error/ze10010"
+        },
+
+        {
+            code: "ZE10020",
+            name: "Package.json in current directory is not a valid json",
+            link: "/guide/error/ze10020"
         },
         {
-            code: "ZE002",
+            code: "ZE10030",
             name: "Package.json doesn't have a name or version field.",
-            link: "/error/ze002"
+            link: "/guide/error/ze10030"
         },
         {
-            code: "ZE003",
-            name: "Wrong package.json found.",
-            link: "/error/ze003"
+            code: "ZE10110",
+            name: "Git remote origin is not configured properly.",
+            link: "/guide/error/ze10110"
+
         },
         {
-            code: "ZE004",
+            code: "ZE10120",
             name: "Error getting git credentials.",
-            link: "/error/ze004"
+            link: "/guide/error/ze10120"
+        }
+
+    ],
+    // deploy specific errors
+    deploy: [
+        {
+            code: "ZE10040",
+            name: "Assets not found.",
+            link: "/guide/error/ze10040"
         },
-        // TODO: Is there any custom errors in the code? 
+        {
+            code: "ZE10041",
+            name: "Assets not found in snapshot",
+            link: "/guide/error/ze10041"
+        },
+        {
+            code: "ZE10042",
+            name: "application_uid is required.",
+            link: "/guide/error/ze10042"
+        },
+        {
+            code: "ZE10043",
+            name: "snapshot_id not found.",
+            link: "/guide/error/ze10043"
+        },
+        {
+            code: "ZE10044",
+            name: "Missing file hash",
+            link: "/guide/error/ze10044"
+        }
+
     ]
+}
+
+
+export const DeployErrors = {
+
 }
