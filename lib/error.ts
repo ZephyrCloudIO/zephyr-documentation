416334fba319968e467c0e83b66b43a9291f5b58
zmzlois <lois.z@comcord.vision>
2024-07-09 11:30:12 +0100

    chore(error): rename custom errors

--- lib/error.ts
+++ lib/error.ts
@@ -1,118 +1,118 @@
 export const Errors = {
-  // build specific errors
-  build: [
+  // Build specific errors
+  Build: [
     {
-      code: "BU10010",
+      code: "ZE10010",
       name: "Package.json not found.",
     },
 
     {
-      code: "BU10011",
+      code: "ZE10011",
       name: "Package.json is not in a valid json format.",
     },
 
     {
-      code: "BU10013",
+      code: "ZE10013",
       name: "Package.json must have a name and version field.",
     },
     {
-      code: "BU10014",
+      code: "ZE10014",
       name: "Git remote origin is not configured properly.",
 
     },
     {
-      code: "BU10015",
+      code: "ZE10015",
       name: "Git username or email is not configured.",
     },
     {
-      code: "BU10016",
+      code: "ZE10016",
       name: "Could not get git info.",
     },
     {
-      code: "BU10017",
+      code: "ZE10017",
       name: "`application_uid` missing.",
     },
     {
-      code: "BU10018",
+      code: "ZE10018",
       name: "Auth error.",
     },
     {
-      code: "BU10019",
-      name: "Could not get build id.",
+      code: "ZE10019",
+      name: "Could not get ZEild id.",
     },
     {
-      code: "BU10020",
+      code: "ZE10020",
       name: "Could not initialize Zephyr Agent.",
     },
     {
-      code: "BU10021",
+      code: "ZE10021",
       name: "Failed to get application hash list.",
     },
 
 
   ],
-  // deploy specific errors
+  // Deploy specific errors
   deploy: [
     {
-      code: "DE20010",
+      code: "ZE20010",
       name: "Assets not found.",
     },
     {
-      code: "DE20011",
+      code: "ZE20011",
       name: "Assets not found in snapshot",
     },
     {
-      code: "DE20012",
+      code: "ZE20012",
       name: "application_uid is required.",
     },
     {
-      code: "DE20013",
+      code: "ZE20013",
       name: "Missing file hash.",
     },
     {
-      code: "DE20014",
+      code: "ZE20014",
       name: "Failed to load application configuration.",
     },
     {
-      code: "DE20015",
-      name: "Failed to upload build stats."
+      code: "ZE20015",
+      name: "Failed to upload ZEild stats."
     },
     {
-      code: "DE20016",
+      code: "ZE20016",
       name: "Did not receive envs from build stats upload."
     },
     {
-      code: "DE20017",
+      code: "ZE20017",
       name: "Failed to upload assets."
     },
     {
-      code: "DE20018",
+      code: "ZE20018",
       name: "Failed to upload snapshots."
     },
 
 
     {
-      code: "DE20019",
+      code: "ZE20019",
       name: "Snapshot uploads gave no results."
     },
     {
-      code: "DE20020",
+      code: "ZE20020",
       name: "Failed to get application hash list."
     },
     {
-      code: "DE20021",
+      code: "ZE20021",
       name: "Could not resolve application name with version."
     },
     {
-      code: "DE20022",
+      code: "ZE20022",
       name: "`snapshot_id` not found."
     },
     {
-      code: "DE20023",
+      code: "ZE20023",
       name: "Snapshot not found."
     },
     {
-      code: "DE20024",
+      code: "ZE20024",
       name: "Could not get build id."
     }
 
@@ -120,6 +120,4 @@ export const Errors = {
 }
 
 
-export const DeployErrors = {
 
-}
