7883531ddc686bb83ab8a452ac6688af3915e6c0
zmzlois <lois.z@comcord.vision>
2024-08-28 02:10:18 +0100

    feat(chore): assets

--- rspress.config.ts
+++ rspress.config.ts
@@ -159,8 +159,8 @@ export default defineConfig({
   mediumZoom: { selector: '.rspress-doc img' },
 
   logo: {
-    light: '/logo-light-bg.png',
-    dark: '/logo-dark-bg.png',
+    light: '/light-bg-icon.png',
+    dark: '/dark-bg-icon.png',
   },
   search: {
     searchHooks: path.join(__dirname, 'lib/utils/after-search.ts'),
