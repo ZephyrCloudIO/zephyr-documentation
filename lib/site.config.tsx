3b9c451655014cf07ec15dc39fcdb8b17c8b59e4
zmzlois <lois.z@comcord.vision>
2024-10-14 09:23:23 +0200

    update

--- lib/site.config.tsx
+++ lib/site.config.tsx
@@ -10,7 +10,7 @@ import { WebpackIcon } from '../components/icons/webpack';
 import { NetlifyIcon } from '../components/icons/netlify';
 import { CloudflareIcon } from '@/components/icons/cloudflare';
 
-export const version = '0.0.17';
+export const version = '0.0.21';
 
 export interface CardItemProps {
   title: string;
@@ -140,10 +140,17 @@ export const SiteConfig: CardProps = {
     {
       title: 'Vite + Rspack + Webpack + Module Federation',
       framework: 'vite',
-      description: 'Deploying a Micro-Frontend application using the Official Vite Plugin from Module Federation with Vite, Rspack and Webpack on Zephyr. ',
+      description:
+        'Deploying a Micro-Frontend application using the Official Vite Plugin from Module Federation with Vite, Rspack and Webpack on Zephyr. ',
       href: '/recipes/vite-rspack-webpack-mf',
       variant: 'small',
-      icons: [<ViteIcon />, <ReactIcon />, <WebpackIcon />, <RspackIcon />, <ModuleFederationIcon />]
+      icons: [
+        <ViteIcon />,
+        <ReactIcon />,
+        <WebpackIcon />,
+        <RspackIcon />,
+        <ModuleFederationIcon />,
+      ],
     },
 
     // {
