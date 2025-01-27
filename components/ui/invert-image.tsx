a2e757cd6b46addfe9009eb4ace8ca19871941db
zmzlois <lois.z@comcord.vision>
2025-01-27 20:23:14 +0800

    feat: repack general knowledge

--- components/ui/invert-image.tsx
+++ components/ui/invert-image.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useDark } from 'rspress/runtime';
+import React from "react";
+import { useEffect, useState } from "react";
+import { useDark } from "rspress/runtime";
 
 interface ImageTypeProps {
-  darkSrc?: string;
-  lightSrc?: string;
+	darkSrc?: string;
+	lightSrc?: string;
 }
 export const InvertImage = ({ darkSrc, lightSrc }: ImageTypeProps) => {
-  const [imageSrc, setImageSrc] = useState('/dark-bg-logo.png');
+	const [imageSrc, setImageSrc] = useState("/dark-bg-logo.png");
 
-  const isDark = useDark();
+	const isDark = useDark();
 
-  useEffect(() => {
-    if (!isDark) setImageSrc(lightSrc ? lightSrc : '/light-bg-logo.png');
+	useEffect(() => {
+		if (!isDark) setImageSrc(lightSrc ? lightSrc : "/light-bg-logo.png");
 
-    if (isDark) setImageSrc(darkSrc ? darkSrc : '/dark-bg-logo.png');
-  }, [isDark, darkSrc, lightSrc]);
+		if (isDark) setImageSrc(darkSrc ? darkSrc : "/dark-bg-logo.png");
+	}, [isDark, darkSrc, lightSrc]);
 
-  return (
-    <img
-      src={imageSrc}
-      alt="Zephyr Cloud"
-      height="100"
-      className="md:w-1/2 w-full md:min-h-[220px] mx-auto"
-    />
-  );
+	return (
+		<img
+			src={imageSrc}
+			alt="Zephyr Cloud"
+			height="100"
+			className="md:w-1/2 w-full md:min-h-[220px] mx-auto"
+		/>
+	);
 };
