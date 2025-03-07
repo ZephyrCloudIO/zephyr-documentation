39f9f902c624b882e105e99542e078a968c3764a
Néstor <nestor@nstlopez.com>
2025-03-08 00:39:15 +0100

    chore: formatting settings (#90)

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -4,22 +4,22 @@ import * as React from "react";
 import { cn } from "../../lib/cn";
 
 const Progress = React.forwardRef<
-	React.ElementRef<typeof ProgressPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
 >(({ className, value, ...props }, ref) => (
-	<ProgressPrimitive.Root
-		ref={ref}
-		className={cn(
-			"relative h-1 w-1/2 overflow-hidden rounded-full bg-zinc-600/40",
-			className,
-		)}
-		{...props}
-	>
-		<ProgressPrimitive.Indicator
-			className="h-full w-full flex-1 bg-emerald-300 transition-all"
-			style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-		/>
-	</ProgressPrimitive.Root>
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-1 w-1/2 overflow-hidden rounded-full bg-zinc-600/40",
+      className,
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-emerald-300 transition-all"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
