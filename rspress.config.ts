988ce80a1722917ed6acc025df82a408e5fb64f4
Valerii Tropin <tropin.valeriy@gmail.com>
2025-06-06 17:17:30 +0300

    feat: akamai integration

--- rspress.config.ts
+++ rspress.config.ts
@@ -154,6 +154,10 @@ const sidebar: Sidebar = {
       link: "/cloud",
       items: [
         {
+          text: "Akamai",
+          link: "/cloud/akamai",
+        },
+        {
           text: "Cloudflare",
           link: "/cloud/cloudflare",
         },
